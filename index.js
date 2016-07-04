23fbb5706fd1f982fcf447abf9ab6a1e019a75bc
Edu <eduwp90@gmail.com>
2016-07-04 19:36:56 +0000

    my first commit

--- index.js
+++ index.js
@@ -4,15 +4,19 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var cors = require('cors');
+var Agenda = require('agenda');
+
+
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI;
+
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://heroku_253vdv8v:4m9qab9skph076ov6sujdjgoir@ds019488.mlab.com:19488/heroku_253vdv8v',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '' , //Add your master key here. Keep it secret! 
@@ -21,6 +25,7 @@ var api = new ParseServer({
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
 });
+var agenda = new Agenda(databaseUri);
 
 var app = express();
 app.use(cors());
@@ -39,3 +44,13 @@ var port = process.env.PORT || 1337;
 app.listen(port, function() {
     console.log('parse-server-example running on port ' + port + '.');
 });
+
+agenda.define('greet the world', function(job, done) {
+  console.log(job.attrs.data.time, 'hello world!');
+  done();
+});
+
+agenda.schedule('every 10 seconds', 'greet the world', {time: new Date()});
+agenda.start();
+
+console.log('Wait 10 seconds...');
