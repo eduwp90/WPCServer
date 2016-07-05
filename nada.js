3ff305ed478b79b03cc3533c75a3b4073de595d1
Edu <eduwp90@gmail.com>
2016-07-05 19:46:22 +0000

    primer later

--- nada.js
+++ nada.js
@@ -1,11 +1,20 @@
 var later = require('later');
 
-  var sched = later.parse.recur().every(1).minute(),
-      t = later.setInterval(test, sched),
-      count = 5;
+
+
+
+  var sched = later.parse.recur().on('20:00:00').time()
+  .and()
+  .on('03:00:00').time()
+  .and()
+  .on('12:00:00').time(),
+ 
+  t = later.setInterval(test, sched),
+      count = 4;
 
   function test() {
     console.log(new Date());
+    console.log("ola k ase");
     count--;
     if(count <= 0) {
       t.clear();
